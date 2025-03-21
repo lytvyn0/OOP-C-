1bff6b62e8ba8f3d763efcd680dfc743a2043bee
lytvyn0 <kn1b23.lytvyniuk@kpnu.edu.ua>
2025-03-21 09:51:27 +0200

    Add files via upload

---
+++ Lab01.lytvyniuk/Lab01.lytvyniuk/1.cs
@@ -0,0 +1,79 @@
+﻿  /* Задача 1. Вивести таблицю квадратів чисел від 1 до 10.
+
+ 
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Число\tКвадрат");
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{i}\t {i * i}");
+        }
+    }
+}
+
+*/
+
+
+
+
+/* Задача 2. Обчислити суму цифр у введеному числі.
+ 
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введiть число:");
+        int number = int.Parse(Console.ReadLine());
+        int sum = 0;
+
+        while (number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        Console.WriteLine("Сума цифр: " + sum);
+    }
+}
+*/
+ 
+
+
+/* Задача 3. Визначити кількість додатних чисел у масиві.
+  
+  
+ 
+using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+        int[] numbers = { 1, -2, 3, 4, -5, 6, -7 };
+
+        int count = 0;
+
+       
+        foreach (int number in numbers)
+        {
+           
+            if (number > 0)
+            {
+                count++;
+            }
+        }
+
+        
+        Console.WriteLine("Кiлькiсть додатнiх чисел у масивi: " + count);
+    }
+}
+
+ */
