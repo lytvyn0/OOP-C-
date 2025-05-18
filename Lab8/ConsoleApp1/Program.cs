d031db107402512404ae39634f582954850c01ab
lytvyn0 <kn1b23.lytvyniuk@kpnu.edu.ua>
2025-05-18 20:29:04 +0300

    Add files via upload

---
+++ Lab8/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string f1 = "f1.txt";
+        string f2 = "f2.txt";
+        string h = "h.txt";
+
+        try
+        {
+
+            File.Copy(f1, h, true);
+            File.Copy(f2, f1, true);
+            File.Copy(h, f2, true);
+
+            Console.WriteLine("Файли успiшно обмiнянi.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Сталася помилка: " + ex.Message);
+        }
+    }
+}
