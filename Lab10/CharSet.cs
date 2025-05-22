097cd70547487f8d5924716ea8e2607a400ddb0f
lytvyn0 <kn1b23.lytvyniuk@kpnu.edu.ua>
2025-05-22 13:26:46 +0300

    Add files via upload

---
+++ Lab10/CharSet.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Перевантаження_операцій
+{
+    }
+public class CharSet
+{
+    private HashSet<char> elements;
+
+    public CharSet()
+    {
+        elements = new HashSet<char>();
+    }
+
+    public CharSet(IEnumerable<char> items)
+    {
+        elements = new HashSet<char>(items);
+    }
+
+    public static CharSet operator +(char c, CharSet set)
+    {
+        var newSet = new CharSet(set.elements);
+        newSet.elements.Add(c);
+        return newSet;
+    }
+
+    public static CharSet operator +(CharSet set1, CharSet set2)
+    {
+        var newSet = new CharSet(set1.elements);
+        foreach (var item in set2.elements)
+        {
+            newSet.elements.Add(item);
+        }
+        return newSet;
+    }
+
+  
+    public static bool operator ==(CharSet set1, CharSet set2)
+    {
+        if (ReferenceEquals(set1, set2))
+            return true;
+        if (ReferenceEquals(set1, null) || ReferenceEquals(set2, null))
+            return false;
+
+        return set1.elements.SetEquals(set2.elements);
+    }
+   
+    public static bool operator !=(CharSet set1, CharSet set2)
+    {
+        return !(set1 == set2);
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is CharSet set && this == set;
+    }
+
+    public override int GetHashCode()
+    {
+        int hash = 19;
+        foreach (var c in elements.OrderBy(e => e))
+        {
+            hash = hash * 31 + c.GetHashCode();
+        }
+        return hash;
+    }
+
+    public override string ToString()
+    {
+        return "{" + string.Join(", ", elements) + "}";
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CharSet set1 = new CharSet(new[] { 'a', 'b', 'c' });
+        CharSet set2 = new CharSet(new[] { 'c', 'd' });
+
+        CharSet unionSet = set1 + set2; 
+        CharSet extendedSet = 'e' + set1; 
+
+        Console.WriteLine("Set1: " + set1);
+        Console.WriteLine("Set2: " + set2);
+        Console.WriteLine("Union: " + unionSet);
+        Console.WriteLine("Extended Set1: " + extendedSet);
+
+        Console.WriteLine("Set1 == Set2: " + (set1 == set2));
+        Console.WriteLine("Set1 == Extended Set1: " + (set1 == extendedSet));
+    }
+}
